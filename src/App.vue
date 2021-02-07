cdd453a53d116705cfabe7af31a39cc15b89ecfa
iobami <ayobamialadenoye@yahoo.com>
2021-02-07 13:08:25 +0000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,80 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="mx-auto" style="width: 40%">
+    <form @submit.prevent="onSubmit()">
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input
+          type="email"
+          class="form-control"
+          aria-describedby="emailHelp"
+          placeholder="Enter email"
+          v-model="register.email"
+        />
+      </div>
+      <div class="form-group">
+        <label for="exampleInputEmail1">first</label>
+        <input
+          type="text"
+          class="form-control"
+          aria-describedby="emailHelp"
+          placeholder="Enter name"
+          v-model="register.firstname"
+        />
+      </div>
+      <div class="form-group">
+        <label for="exampleInputEmail1">last</label>
+        <input
+          type="text"
+          class="form-control"
+          aria-describedby="emailHelp"
+          placeholder="Enter name"
+          v-model="register.lastname"
+        />
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          placeholder="Password"
+          v-model="register.password"
+        />
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      register: {
+        email: 'ayobami@gmail.com',
+        firstname: '',
+        lastname: '',
+        password: '',
+        id: 122594659
+      }
+    };
+  },
+
+  methods: {
+    onSubmit() {
+      axios.post('https://mysql-golang-app.herokuapp.com/user/register', this.register)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
   }
-}
+};
 </script>
 
 <style>
